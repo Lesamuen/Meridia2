b69ca45f60d256ef52d9a1423ae2cc3b81fd19b2
Samuel Nguyen <lesamuengu@gmail.com>
2023-10-03 06:11:20 -0700

    Update documentation and indent logging

--- modules/auxiliary.py
+++ modules/auxiliary.py
@@ -9,17 +9,21 @@ from asyncio import sleep
 
 def getTime() -> str:
     '''
-    Returns the current time in extended ISO8601 format
+    Returns the current time in extended ISO8601 format; 20 chars long
     '''
 
     return datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
 
 
-def log(out: str):
+def log(out: str) -> None:
     '''
     Both prints the input string to the console and writes the input string to a dated log.
 
     This log is found in the logs/ folder (the logs folder has to be created first).
+
+    ### Parameters
+    out: str
+        String to print to file and console
     '''
 
     print(out)
@@ -28,11 +32,18 @@ def log(out: str):
         log_file.write(out + "\n")
 
 
-async def playAudio(vc: VoiceChannel, file: str):
+async def playAudio(vc: VoiceChannel, file: str) -> None:
     '''
-    Has the bot join a specified voice channel, and play an audio clip from ./audio/<file>.ogg
+    Has the bot join a voice channel, and play an audio clip from ./audio/<file>.ogg
 
     Bot automatically leaves after audio stops playing.
+
+    ### Parameters
+    vc: VoiceChannel
+        Voice channel to join
+    
+    file: str
+        Name of audio file to play (excluding .ogg)
     '''
 
     if vc is None:
@@ -51,14 +62,14 @@ async def playAudio(vc: VoiceChannel, file: str):
 
     # Detect failure to obtain voice client
     if voice_client is None:
-        log("ERROR >> Audio commands have intersected!")
+        log("ERROR HAS OCCURRED   >> Audio commands have intersected!")
         return
 
     # Load audio
     try:
         audio: FFmpegOpusAudio = FFmpegOpusAudio("audio/" + file + ".ogg", codec = "copy")
     except ClientException:
-        log("ERROR >> Audio file not found!")
+        log("ERROR HAS OCCURRED   >> Audio file not found!")
         return
     
     # Play audio
@@ -69,6 +80,6 @@ async def playAudio(vc: VoiceChannel, file: str):
                 log(getTime() + " >> Disconnecting voice client in GUILD[" + str(voice_client.guild) + "]")
                 await voice_client.disconnect()
         except ClientException:
-            log("ERROR >> Audio commands have collided!")
+            log("ERROR HAS OCCURRED   >> Audio commands have collided!")
     else:
         voice_client.source = audio
